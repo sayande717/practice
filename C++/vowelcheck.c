68bc4f7fd837864a620bb054c396d9df4e5a216c
sayande717 <sayande717@outlook.in>
2021-04-29 12:15:49 +0530

    .

--- C++/vowelcheck.c
+++ C++/vowelcheck.c
@@ -5,6 +5,6 @@ void main()
     printf("Enter a character:");
     scanf("%c",&n);
     if(n=='A'||n=='a'||n=='E'||n=='e'||n=='I'||n=='i'||n=='O'||n=='o'||n=='U'||n=='u')
-    printf("%c is a vowel",n);
-    else printf("%c is a consonant",n);
+    printf("%c is a vowel.",n);
+    else printf("%c is not a vowel.",n);
 }
