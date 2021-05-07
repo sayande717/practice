d75be65478475510de26dd228a0da23d506f89db
sayande717 <sayande4203@protonmail.com>
2021-05-07 10:52:47 +0530

    .

--- C++/concatenate 2 strings.c
+++ C++/concatenate 2 strings.c
@@ -1,14 +1,16 @@
+//Program to concatenate 2 strings.
 #include<stdio.h>
 #include<limits.h>
 void main()
 {
     /*Variable descriptions - 
-    s1[INT_MAX],s2[INT_MAX] = Strings 1 & 2.
+    string1,string2 = Strings 1 & 2.
     i = counter variable used in for loop.
     */
-    char s1[INT_MAX], s2[INT_MAX];
+    char string1[INT_MAX], string2[INT_MAX];
     int i;
     printf("Enter 1st string.");
-    for(i=0;i<)
-
-}
+    scanf("%s",&string1);
+    printf("Enter 2nd string.");
+    scanf("%s",&string2);
+    //not complete
