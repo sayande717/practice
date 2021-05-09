baf9e3d291be157bab7559acb799ae023a59aa3a
sayande717 <sayande4203@protonmail.com>
2021-05-09 14:01:54 +0530

    9.5.21 till 2pm

--- C++/smallest no from array.c
+++ C++/smallest no from array.c
@@ -6,18 +6,22 @@ void main()
     /*Variables used - 
     a = The array,n = number of elements of array.
     i = Counter variable used used in the for loop.
+    min = Stores the smallest number.
     pos = Stores the position of the smallest number.
     */
-    int n,min=INT_MAX,i,pos;
-    printf("Enter number of array elements.");
-    scanf("%d",&n);
+    int n,min=INT_MAX,i,pos=0;
+    printf("Enter number of array elements."); 
+    scanf("%d",&n); //Takes no. of array elements as input.
     int a[n];
     printf("Enter array elements.");
     for(i=0;i<n;i++)
     {
-        scanf("%d",&a[i]);
-        if(a[i]<min)
-        pos=(i+1);
+        scanf("%d",&a[i]);  //Takes array elements as input.
+        if(a[i]<min) //Checks if the element is smaller than 'min' & stores it's position if true.
+        {
+            min=a[i];
+            pos=(i+1);
+        }
     }
-    printf("Position of smallest number:%d",pos);
+    printf("Position of smallest number:%d",pos); //Prints the position of the smallest number.
 }
---
+++ C++/thief policeman.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void main()
+{
+    /*Variable descriptions - 
+    n = the total number of thieves and policemen.
+    k = an integer.
+    a[n] = The array containing the thieves and policemen.
+    c = An integer which counts the number of policemen.
+    i = counter variable used in for loop.
+    */
+    int n,k;
+    //Enter the value of n & k.
+    scanf("%d %d",&n,&k);
+    if(n<1000&&k<100)
+    {
+        int a[n],c=0,i;
+        for(i=0;i<n;i++)
+        {
+            //Enter the array elements.
+            scanf("%d ",&a[i]);
+            if(a[i]==1)
+            c++;
+        }
+        printf("%d",c);
+    }
+    else printf("Constraints not met.");
+}
