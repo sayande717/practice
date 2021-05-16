2474d3cfdae385e44231da03fb776e9459f88bcd
sayande717 <sayande4203@protonmail.com>
2021-05-16 12:47:30 +0530

    16/5/21 till 1pm

--- C++/largest, smallest no from array.c
+++ C++/largest, smallest no from array.c
@@ -6,35 +6,25 @@ void main()
     /*Variables used - 
     a = The array,n = number of elements of array.
     i = Counter variable used used in the for loop.
-    min,max = Stores the largest & smallest numbers in the array.
+    min,max = Stores the position of largest & smallest numbers in the array.
     s = Store a temporary value, needed to swap the values of min & max.
     */
-    int n,min=INT_MAX,max=INT_MIN,i,s;
+    int n,max=0,i,s;
     printf("Enter number of array elements.");
     scanf("%d",&n);
-    int a[n];
+    int a[n],min=(n-1);
     printf("Enter array elements.");
     for(i=0;i<n;i++)
     {
         scanf("%d",&a[i]);
-        if(a[i]<min)
-        min=a[i];
-        if(a[i]>max)
-        max=a[i];
+        if(a[i]<a[min])
+        min=i;
+        if(a[i]>a[max])
+        max=i;
     }
-    /*print original array if necessary.
-    printf("\nOriginal array:");
-    for(i=0;i<n;i++)
-    printf("%d\t",a[i]);
-    */
     printf("\nOriginal elements:\nSmallest=%d\tLargest=%d",a[min],a[max]);
     s=a[min];
     a[min]=a[max];
     a[max]=s;
-    printf("\nInterchanged elements:\nSmallest=%d\tLargest=%d",a[min],a[max]);
-    /*print modified array if necessary.
-    printf("\nModified array:");
-    for(i=0;i<n;i++)
-    printf("%d\t",a[i]);
-    */
+    printf("\nInterchanged elements:\nSmallest=%d\tLargest=%d\n",a[min],a[max]);
 }
