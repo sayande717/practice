07a71324d1a4b5297c6620365cbca8c542920420
sayande717 <sayande4203@protonmail.com>
2021-05-17 12:51:11 +0530

    17.5.21 till 1pm

---
+++ C++/matrixtranspose.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+void main()
+{
+    /*Variable description - 
+    a[3][3] = Original Matrix.
+    b[3][3] = Transposed Matrix.
+    i,j = Counter variables used in for loop.
+    */
+    int a[3][3],b[3][3],i,j;
+    printf("Enter elements of the matrix:");
+    for(i=0;i<3;i++)
+    {
+        printf("\n[Row %d]\n",i+1);
+        for(j=0;j<3;j++)
+        {
+            printf("Column %d:",j+1);
+            scanf("%d",&a[i][j]);
+            a[i][j]=b[j][i];
+        }
+    }
+    printf("Transposed Matrix:\n");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        printf("%d\t",b[i][j]);
+        printf("\n");
+    }
+}
