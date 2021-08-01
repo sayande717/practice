4d4ec9b828423c3a36a37c4bf6ec2e1e715e86d2
sayande717 <sayande4203@protonmail.com>
2021-08-01 13:05:11 +0530

    .

--- C/21.c
+++ C/21.c
@@ -22,7 +22,7 @@ void main()
             printf("Column %d : ",(j+1));
             scanf("%d",&a[i][j]); //Take the elements of the array as input.
             if(i==j) //Check if row number = column number.
-            s=s+a[i][j]; //Adds the elements which satisfy the condition ie row=column.
+            s=s+a[i][j]; //Adds the elements which satisfy the condition ie row = column.
         }
     }
     printf("\nThe trace of the square matrix is %d.",s); //Print the trace of the matrix.
