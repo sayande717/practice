a00a079b56f1ae3759a8d3e17965069b755929b9
Sayan De <sayande4203@protonmail.com>
2021-12-05 12:54:19 +0530

    Rectified 4.c

--- C/4.c
+++ C/4.c
@@ -4,7 +4,7 @@ void main()
     char n;
     printf("Enter an alphabet:");
     scanf("%c",&n);
-    if(n!='A'&&n!='a'&&n!='E'&&n!='e'&&n!='I'&&n!='i'&&n!='O'&&n!='o'&&n!='U'&&n!='u')
-    printf("%c is a consonant",n);
-    else printf("%c is not a consonant",n);
+    if(n=='A'||n=='a'||n=='E'||n=='e'||n=='I'||n=='i'||n=='O'||n=='o'||n=='U'||n=='u')
+    printf("%c is a vowel.\n",n);
+    else printf("%c is a consonant.\n",n);
 }
