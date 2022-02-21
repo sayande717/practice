4a7e6f721490b8f5cfb250a9a439e1858533ddc1
sayande717 <sayande4203@protonmail.com>
2022-02-21 20:07:51 +0530

    .

--- Python/Discord Bot/local.py
+++ Python/Discord Bot/local.py
@@ -6,14 +6,23 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    await client.change_presence(status='online')
+    await client.change_presence(status='online',activity=discord.Activity(type=discord.ActivityType.listening, name='%help'))
     print('Local bot logged in.')
 
+#Replying to messages
 @client.event
 async def on_message(message):
     if message.content.find('Hi') != -1 or message.content.find('hi') != -1:
         await message.reply('Hello there!')
     if message.content.find('Ping') != -1 or message.content.find('ping') != -1:
         await message.reply('Pong!')
+        
+#Help menus
+@client.event
+async def on_message(message):
+    if message.channel.id == 944823432971968523 or message.channel.id == 944825825243574292:
+        if message.content.find('%help') != -1:
+            if message.content.find('music') != -1:
+                await message.reply('---Help Menu---\n1. Hydra = **.help** \n2. FredBoat = **;;help** \n3. Lofi Radio: = **lofi.help** & **/help**')
 
 client.run(TOKEN)
--- Python/Discord Bot/test.py
+++ Python/Discord Bot/test.py
@@ -7,13 +7,24 @@ TOKEN = os.getenv('Test_token')
 
 client = discord.Client()
 
-#Status: Active
+#Status: Idle
 
+@client.event
+async def on_ready():
+    await client.change_presence(status='idle')
+    print('Test bot idling...')
+
+#Status: Active
+'''
 @client.event
 async def on_ready():
     await client.change_presence(status='online',activity=discord.Activity(type=discord.ActivityType.listening, name='Admin'))
     print('Test bot logged in.')
-
-#Test code
+#Push the update
+@client.event
+async def on_message(message):
+    if message.content.find('$up') != -1:
+        await message.channel.send('[21.02.22 10:36]:')
+'''
 
 client.run(TOKEN)
