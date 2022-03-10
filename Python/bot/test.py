bef33394affdfc0cf43cc2e16bc6e0268f6a0f58
sayande717 <sayande4203@protonmail.com>
2022-03-10 08:34:43 +0530

    Added test bot idle status

--- Python/bot/test.py
+++ Python/bot/test.py
@@ -6,15 +6,21 @@ load_dotenv()
 TOKEN = os.getenv('Test_token')
 client = discord.Client()
 
+#Status: Idle
+@client.event
+async def on_ready():
+    await client.change_presence(status='idle')
+    print('Test bot idling...')
+'''
 #Status: Active
 @client.event
 async def on_ready():
     await client.change_presence(status='online',activity=discord.Activity(type=discord.ActivityType.listening, name='Admin'))
     print('Test bot running.')
     #Send the bot update message(s).
-    #channel = client.get_channel(940487185973530655)
-    #await channel.send("[07.03.22 17:10]:\n")
-
+    #channel = client.get_channel(944658998550999060)
+    #await channel.send("[07/03/2022 | 22:38]:\n")
+'''
 #Test code
 @client.event
 async def on_message(message):
