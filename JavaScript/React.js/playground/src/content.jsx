21983b1fd407c4c2449d023fd0493895895c1bc7
sayande717 <sayande717@github.com>
2023-03-13 11:24:41 +0530

    .

--- JavaScript/React.js/playground/src/content.jsx
+++ JavaScript/React.js/playground/src/content.jsx
@@ -1,4 +1,4 @@
-export function Content() {
+function Content() {
     return  (
         <div>
             <ol id="font-fix">
@@ -8,3 +8,5 @@ export function Content() {
         </div>
     )
 }
+
+export default Content
--- JavaScript/React.js/playground/src/index.jsx
+++ JavaScript/React.js/playground/src/index.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { Header } from './header';
-import { Content } from './content';
-import { Footer } from './footer';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render (  
     <div>
-        <Header />
-        <Content />
-        <Footer />
+        <App />
     </div>
 )
